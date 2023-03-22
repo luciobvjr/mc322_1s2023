4d995bedf3209e76efc5da63e7052544bbb3d695
Lucio Bueno Vieira Junior <ra221029@zero.lab.ic.unicamp.br>
2023-03-21 22:28:30 -0300

    Classe 'Cliente'

---
+++ seguradora/src/Cliente.java
@@ -0,0 +1,58 @@
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private Integer idade;
+    private String endereco;
+
+    // CONSTRUCTOR
+    public Cliente(String nome, String cpf, String dataNascimento, Integer idade, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.idade = idade;
+        this.endereco = endereco;    
+    }
+
+    // GETTERS
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public String getDataNascimento() {
+        return this.dataNascimento;
+    }
+
+    public Integer getIdade() {
+        return this.idade;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+    
+    // SETTERS
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
