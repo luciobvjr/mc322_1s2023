6ca141ecda17ca555a19f9c7b8d85e95b208ba5b
Lucio Bueno Vieira Junior <ra221029@zero.lab.ic.unicamp.br>
2023-03-21 21:47:16 -0300

    classe 'Seguradora'

---
+++ seguradora/src/Seguradora.java
@@ -0,0 +1,38 @@
+public class Seguradora {
+    String nome;
+    String telefone;
+    String email;
+    String endereco;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
